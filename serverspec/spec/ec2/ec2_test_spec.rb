b7ac123789a239e864478aa6a96ec055432076c9
mkmmr <k.vanilla.0830@gmail.com>
2023-04-25 00:46:19 +0900

    [ci skip] serverspecのテンプレートを追加

---
+++ serverspec/spec/ec2/ec2_test_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+listen_port = 80
+
+describe package('nginx') do
+    it { should be_installed }
+end
+
+describe port(listen_port) do
+    it { should be_listening }
+end
+
+describe command('curl http://127.0.0.1:#{listen_port}/_plugin/head/ -o /dev/null -w "%{http_code}\n" -s') do
+    its(:stdout) { should match /^200$/ }
+end
+
+describe command('ruby -v') do
+    its(:stdout) { should match /ruby 3\.1\.2/ }
+end
